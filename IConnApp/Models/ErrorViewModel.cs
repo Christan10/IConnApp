bcb9acc4c631c53fbc4a7706239d4a7448d2c178
Christos Skordas <christanskordas@gmail.com>
2018-07-21 16:54:16 +0100

    init commit

---
+++ IConnApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IConnApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
